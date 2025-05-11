1baef16a69366aa185c7f74720f6b80e3a2d2df8
Dan McClory <danmcclory@fastmail.com>
2025-05-11 11:58:13 -0400

    more consistent error handling in the database/Gorm code

--- cmd/database.go
+++ cmd/database.go
@@ -4,6 +4,7 @@ import (
 	"github.com/glebarez/sqlite"
 	"gorm.io/gorm"
 	"time"
+	"fmt"
 	"gorm.io/gorm/logger"
 )
 
@@ -23,7 +24,7 @@ func loadDb(path string) (*gorm.DB, error) {
 		Logger: logger.Default.LogMode(logger.Silent),
 	})
 	if err != nil {
-		return &gorm.DB{}, err
+		return &gorm.DB{}, fmt.Errorf("loadDb: failed executing query: %w", err)
 	}
 	return db, nil
 }
@@ -32,7 +33,7 @@ func initDb(path string) (*gorm.DB, error) {
 	db, err := loadDb(path)
 
 	if err != nil {
-		return &gorm.DB{}, err
+		return &gorm.DB{}, fmt.Errorf("initDb: failed executing query: %w", err)
 	}
 
 	db.AutoMigrate(&Option{})
@@ -44,18 +45,19 @@ func initDb(path string) (*gorm.DB, error) {
 func loadOptions(db *gorm.DB) ([]Option, error) {
 	var options []Option
 	err := db.Find(&options).Error
-	return options, err
+	if err != nil {
+		return []Option{}, fmt.Errorf("loadOptions: failed executing query: %w", err)
+	}
+	return options, nil
 }
 
 func getOption(db *gorm.DB, optionId uint) (Option, error) {
 	var option Option
 
 	err := db.First(&option, optionId).Error
-
 	if err != nil {
-		return Option{}, err
+		return Option{}, fmt.Errorf("getOption: failed executing query: %w", err)
 	}
-
 	return option, nil
 }
 
@@ -63,7 +65,7 @@ func addOption(db *gorm.DB, newOption string) (Option, error) {
 	var option Option
 	err := db.FirstOrCreate(&option, Option{Label: newOption}).Error
 	if err != nil {
-		return Option{}, err
+		return Option{}, fmt.Errorf("addOption: failed executing query: %w", err)
 	}
 	return option, nil
 }
@@ -77,9 +79,8 @@ func addOptions(db *gorm.DB, newOptions []string) ([]Option, error) {
 
 	err := db.Create(&optionInputs).Error
 	if err != nil {
-	  return[]Option{}, err
+	  return[]Option{}, fmt.Errorf("addOptions: failed executing query: %w", err)
 	}
-
 	return optionInputs, nil
 }
 
@@ -88,51 +89,69 @@ func removeOption(db *gorm.DB, optionId uint) error {
 	// "After a chain of methods, it’s crucial to check the Error field"
 	err := db.Delete(&Option{}, optionId).Error
 
-	return err
+	if err != nil {
+		return fmt.Errorf("removeOption: failed executing query: %w", err)
+	}
+	return nil
 }
 
 func addVote(db *gorm.DB, winnerId uint, loserId uint) error {
 	createdAt := time.Now().Unix()
 	vote := Vote{WinnerId: winnerId, LoserId: loserId, CreatedAt: createdAt}
 
-	result := db.Create(&vote).Error
+	err := db.Create(&vote).Error
 
-	return result
+	if err != nil {
+		return fmt.Errorf("addVote: failed executing query: %w", err)
+	}
+	return nil
 }
 
 func loadVotes(db *gorm.DB) ([]Vote, error) {
 	var votes []Vote
 	err := db.Find(&votes).Error
 
-	return votes, err
+	if err != nil {
+	  return[]Vote{}, fmt.Errorf("loadVotes: failed executing query: %w", err)
+	}
+	return votes, nil
 }
 
 func deleteVotes(db *gorm.DB, optionId uint) (int64, error) {
 	result := db.Where("winner_id = ? or loser_id = ?", optionId, optionId).Delete(&Vote{})
 
-	return result.RowsAffected, result.Error
+	err := result.Error
+
+	if err != nil {
+	  return 0, fmt.Errorf("deleteVotes: failed executing query: %w", err)
+	}
+
+	return result.RowsAffected, nil
 }
 
 func deleteOptionAndVotes(db *gorm.DB, optionId uint) error {
-	// q := query.Use(db)
-
 	tx := db.Begin()
 
 	_, err := deleteVotes(tx, optionId)
 
 	if err != nil {
 		tx.Rollback()
-		return err
+	  return fmt.Errorf("deleteOptionAndVotes: failed executing query: %w", err)
 	}
 
 	err = removeOption(tx, optionId)
 
 	if err != nil {
 		tx.Rollback()
-		return err
+	  return fmt.Errorf("deleteOptionAndVotes: failed executing query: %w", err)
 	}
 
-	tx.Commit()
+	err = tx.Commit().Error
+
+	if err != nil {
+		tx.Rollback()
+	  return fmt.Errorf("deleteOptionAndVotes: failed executing query: %w", err)
+	}
 
 	return nil
 }
